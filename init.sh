309dec9d5a909112f641c25dcaf454bf2a198b78
Kenta Kase <kesin1202000@gmail.com>
2021-07-04 19:32:50 +0900

    Support docker-compose scale and docker outside of docker

--- init.sh
+++ init.sh
@@ -3,12 +3,18 @@ set -xe
 
 ORG="${GITHUB_ORG}"
 URL="${RUNNER_URL}"
-NAME="${RUNNER_NAME}"
 GROUP="${RUNNER_GROUP:=Default}"
 LABELS="${RUNNER_LABELS}"
 WORKDIR="${RUNNER_WORKDIR:=/tmp/_work}"
 
-CONFIG_CACHE_DIR="config_cache"
+# Detect container name assigned by doker-compose up --scale
+HOSTNAME=$(hostname)
+CONTAINER_NAME="$(sudo docker ps -f "ID=${HOSTNAME}" --format "{{.Names}}")"
+
+# Docker outside of docker needs root user permission
+export RUNNER_ALLOW_RUNASROOT=1
+
+CONFIG_CACHE_DIR="config_cache/${CONTAINER_NAME}"
 
 # Restore runner config from host
 if [ ! -z "$(ls -A ${CONFIG_CACHE_DIR})" ]; then
@@ -22,17 +28,19 @@ fi
 
 if [ ! -e ".runner" ]; then
   RUNNER_TOKEN="$(GITHUB_TOKEN=${GITHUB_ORG_TOKEN} gh api -X POST orgs/${ORG}/actions/runners/registration-token | jq -r .token)"
+
   ./config.sh \
     --unattended \
     --url "${URL}" \
     --token "${RUNNER_TOKEN}" \
-    --name "${NAME}" \
+    --name "${CONTAINER_NAME}" \
     --runnergroup "${GROUP}" \
     --labels "${LABELS}" \
     --work "${WORKDIR}" \
     --replace
 
   # Store runner config to host
+  sudo mkdir -p "${CONFIG_CACHE_DIR}"
   sudo cp .runner "${CONFIG_CACHE_DIR}/"
   sudo cp .credentials* "${CONFIG_CACHE_DIR}/"
   sudo cp .path "${CONFIG_CACHE_DIR}/"
