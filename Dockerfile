bcc1093b5e1b06550b4eaad932083dbafd5513ef
Kenta Kase <kesin1202000@gmail.com>
2021-07-04 17:31:29 +0900

    Register runner automatically

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM cimg/base:edge
 
+RUN curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+  && sudo apt update \
+  && sudo apt install gh \
+  && sudo apt clean
+
 WORKDIR /actions-runner
 RUN curl -o actions-runner-linux-x64-2.278.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.278.0/actions-runner-linux-x64-2.278.0.tar.gz \
   && tar xzf ./actions-runner-linux-x64-2.278.0.tar.gz \
--- init.sh
+++ init.sh
@@ -1,20 +1,42 @@
 #!/usr/bin/env bash
+set -xe
 
+ORG="${GITHUB_ORG}"
 URL="${RUNNER_URL}"
-TOKEN="${RUNNER_TOKEN}"
 NAME="${RUNNER_NAME}"
 GROUP="${RUNNER_GROUP:=Default}"
 LABELS="${RUNNER_LABELS}"
-WORKDIR="${RUNNER_WORKDIR:=_work}"
+WORKDIR="${RUNNER_WORKDIR:=/tmp/_work}"
 
-./config.sh \
-  --unattended \
-  --url "${URL}" \
-  --token "${TOKEN}" \
-  --name "${NAME}" \
-  --runnergroup "${GROUP}" \
-  --labels "${LABELS}" \
-  --work "${WORKDIR}" \
-  --replace
+CONFIG_CACHE_DIR="config_cache"
+
+# Restore runner config from host
+if [ ! -z "$(ls -A ${CONFIG_CACHE_DIR})" ]; then
+  sudo cp "${CONFIG_CACHE_DIR}/.runner" ./
+  sudo cp "${CONFIG_CACHE_DIR}/.credentials" ./
+  sudo cp "${CONFIG_CACHE_DIR}/.credentials_rsaparams" ./
+  sudo chmod +r ".credentials_rsaparams"
+  sudo cp "${CONFIG_CACHE_DIR}/.path" ./
+  sudo cp "${CONFIG_CACHE_DIR}/.env" ./
+fi
+
+if [ ! -e ".runner" ]; then
+  RUNNER_TOKEN="$(GITHUB_TOKEN=${GITHUB_ORG_TOKEN} gh api -X POST orgs/${ORG}/actions/runners/registration-token | jq -r .token)"
+  ./config.sh \
+    --unattended \
+    --url "${URL}" \
+    --token "${RUNNER_TOKEN}" \
+    --name "${NAME}" \
+    --runnergroup "${GROUP}" \
+    --labels "${LABELS}" \
+    --work "${WORKDIR}" \
+    --replace
+
+  # Store runner config to host
+  sudo cp .runner "${CONFIG_CACHE_DIR}/"
+  sudo cp .credentials* "${CONFIG_CACHE_DIR}/"
+  sudo cp .path "${CONFIG_CACHE_DIR}/"
+  sudo cp .env "${CONFIG_CACHE_DIR}/"
+fi
 
 ./run.sh --once
